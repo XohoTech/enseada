cabec98fcba6d99138f9860813203f8dfd139655
Matteo Joliveau <matteo.joliveau@mikamai.com>
2020-04-15 20:50:56 +0200

    Add required permissions to OpenAPI spec and render them with ReDoc
    Signed-off-by: Matteo Joliveau <matteo.joliveau@mikamai.com>

--- build.rs
+++ build.rs
@@ -1,7 +1,27 @@
+use std::env;
+use std::fs;
+use std::path::Path;
 use std::process::Command;
+
+use glob::glob;
 
 fn main() {
     yarte::recompile::when_changed();
 
     Command::new("yarn").arg("docs:build").status().unwrap();
+
+
+    for entry in glob("./docs/openapi/**/*").unwrap() {
+        match entry {
+            Ok(path) => println!("cargo:rerun-if-changed={}", path.display()),
+            Err(e) => panic!(e)
+        }
+    }
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let curr_dir = env::current_dir().unwrap();
+    let dest_dir = Path::new(&out_dir).join("casbin_model.conf");
+    let source_dir = curr_dir.join("config/casbin_model.conf");
+    println!("cargo:rerun-if-changed={}", &source_dir.to_str().unwrap());
+    fs::copy(source_dir, dest_dir).unwrap();
 }
